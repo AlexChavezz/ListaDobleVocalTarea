6522b82162b41caa1d7571179d4058ea101236ef
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-03-09 10:57:48 -0600

    Add files via upload

---
+++ cListaDoble.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+class cListaDoble {
+protected:
+	class Node {
+		char vocal;
+		Node* siguiente;
+		Node* anterior;
+	};
+	Node* list;
+public:
+	cListaDoble();
+	void crearLista();
+	void print();
+};
+
+cListaDoble::cListaDoble() {
+	list = NULL;
+}
+
+void cListaDoble::print() {
+	
+}
+
+void cListaDoble::crearLista() {
+	cListaDoble* aux = NULL,* aux1 = NULL;
+	
+	for (int i = 0; i < 5; i++)
+	{
+		if ( list == NULL ){
+			list = new Node();
+			cout << "Dame la vocal para primer primer Nodo: ";
+			cin >> list->vocal;
+		}
+	}
+
+}
+
+
+int main() {
+
+	cListaDoble* object = new cListaDoble();
+	object->print();
+
+	return 0;
+}
