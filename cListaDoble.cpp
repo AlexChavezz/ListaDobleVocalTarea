832552a213603493abe30bd3a6c00abcaef3d24a
AlexChavezz <76461142+AlexChavezz@users.noreply.github.com>
2022-03-09 11:40:44 -0600

    Update cListaDoble.cpp

--- cListaDoble.cpp
+++ cListaDoble.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 class cListaDoble {
-protected:
+private:
 	class Node {
-		public:
+	public:
 		char vocal;
 		Node* siguiente;
 		Node* anterior;
@@ -14,6 +14,7 @@ public:
 	cListaDoble();
 	void crearLista();
 	void print();
+	bool isChar(char);
 };
 
 cListaDoble::cListaDoble() {
@@ -21,28 +22,66 @@ cListaDoble::cListaDoble() {
 }
 
 void cListaDoble::print() {
-	
+	Node* aux;
+	for (aux = list; aux != NULL; aux = aux->siguiente) {
+		cout << aux->vocal;
+	}
 }
 
 void cListaDoble::crearLista() {
-	cListaDoble* aux = NULL,* aux1 = NULL;
-	
+	Node* aux = NULL,* aux1 = NULL;
+	char letter;
 	for (int i = 0; i < 5; i++)
 	{
 		if ( list == NULL ){
 			list = new Node();
 			cout << "Dame la vocal para primer primer Nodo: ";
 			cin >> list->vocal;
+			aux = list;
+			aux1 = list;
+		}else {
+			aux->siguiente = new Node();
+			aux = aux->siguiente;
+			aux->anterior = aux1;
+			aux1 = aux1->siguiente;
+			cout<<"Dame la vocal de la siguiente vocal: ";
+			cin >> letter;
+			if (isChar(letter)) {
+				aux->vocal = letter;
+			}else {
+				cout << "numero no valido" << endl;
+			}
 		}
 	}
-
 }
-
+bool isChar(char letter) {
+	switch (letter) {
+	
+	case 'a':
+		return true;
+		break;
+	case 'e':
+		return true;
+		break;
+	case 'i':
+		return true;
+		break;
+	case 'o':
+		return true;
+		break;
+	case 'u':
+		return true;
+		break;
+	default:
+		return false;
+		break;
+	}
+}
 
 int main() {
 
 	cListaDoble* object = new cListaDoble();
+	object->crearLista();
 	object->print();
-
 	return 0;
 }
